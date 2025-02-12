4d871c368abc29ff204c1b89a88fc9bb0c2888b2
az707z <az_707z@hotmail.com>
2025-02-12 16:10:58 +0300

    Create file.c

---
+++ file.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+char name[50];
+int i;
+FILE*fptr;
+fptr=fopen("output.txt","w");
+for(i=0;i<10;i++)
+{
+printf("enter name %d:",i+1);
+scanf("%s", name);
+fprintf(fptr,"%s\n",name);
+}
+fclose(fptr);
+fptr=fopen("output.txt","r");
+i=0;
+while(fscanf(fptr,"%s",name)!=EOF)
+{
+printf("%s\n", name);
+i++;
+}
+fclose(fptr);
+return 0;
+}
